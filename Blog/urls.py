c4812625a37a45c2d28f94809c5109eb27a94880
ozgur <ozgur.ersoz3@gmail.com>
2019-12-18 14:03:03 +0300

    user app oluşuruldu

--- Blog/urls.py
+++ Blog/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about, name="about"),
     path('articles/', include("article.urls")),
+    path('user/', include("user.urls")),
    
 ]
